0f66785a7dc30cd28e14c7c5063a2c51b781e39c
Mauri <maumenachotri224@gmail.com>
2024-11-26 01:06:48 -0300

    Dashboard

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import "./Dashboard.css"
+import React from "react";
+import "./Dashboard.css";
+import Image from "next/image";
+
 const Dashboard = () => {
   return (
-    <div className='dashboard'>
-
+    <div className="dashboard">
+      <div className="apartado">
+        <p className="titapartado">Reporte de Platos</p>
+        <div className="busqueda">
+          <div className="cajabusqueda">
+            <Image
+              className="lupita"
+              src="/Vector - 0.png"
+              width={500}
+              height={500}
+              alt={"Logo Hotel Pairumani"}
+            />
+            <p className="textocaja">Buscar Fecha</p>
+          </div>
+        </div>
+      </div>
+      <div className="ContenidoDashboard"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
