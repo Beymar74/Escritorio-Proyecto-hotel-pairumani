cc6834da3abbfe857a1772a061629deb4d593d62
Mauri <maumenachotri224@gmail.com>
2024-11-27 09:42:57 -0300

    LoginV4

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i89.servimg.com"], // Agrega el dominio desde el que cargas la imagen
+    domains: ["i89.servimg.com",'cloudflare-ipfs.com'], // Agrega el dominio desde el que cargas la imagen
   },
 };
 
